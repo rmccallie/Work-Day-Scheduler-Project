febe01eff37e70033d11284507d63671b50b845e
HCTFuzz <rmccallie@hotmail.com>
2021-10-09 11:20:49 -0400

    added date and real-time local clock

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,14 @@
-console.log("Test")
+console.log('Test')
+var today = moment();
+$('#currentDay').text(today.format('MMM Do, YYYY', true));
+var time = moment();
+$('#currentTime').text(time.format('LTS'));
+
+function update() {
+    $('#currentTime').html(moment().format('LTS'));
+  }
+  
+  setInterval(update, 1000);
+
+
+
