1b02a4c3081a7615d6eb666940f2d0046f3cb1ba
HCTFuzz <rmccallie@hotmail.com>
2021-10-11 07:11:50 -0400

    added functions for save buttons and event description classes

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,3 @@
-console.log('Test')
 var today = moment();
 $('#currentDay').text(today.format('MMM Do, YYYY', true));
 var time = moment();
@@ -8,7 +7,46 @@ function update() {
     $('#currentTime').html(moment().format('LTS'));
   }
   
-  setInterval(update, 1000);
+    setInterval(update, 1000);
+
+    $(document).ready(function(){
+
+    $(".saveBtn").on("click", function(){
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(text, time);
+    })
+    
+    function updateTime(){
+        var currentTime = moment().hour();
+        console.log(currentTime)
+        $(".description").each(function(){
+            var hour = parseInt(
+                $(this).attr("id").split("hour")
+            )
+                if (hour < currentTime){
+                    $(this).removeClass("present");
+                    $(this).removeClass("future");
+                    $(this).addClass("past");
+                }
+                else if (hour === currentTime){
+                    $(this).removeClass("future");
+                    $(this).removeClass("past");
+                    $(this).addClass("present");
+                }
+                else {
+                    $(this).removeClass("past");
+                    $(this).removeClass("present");
+                    $(this).addClass("future");
+                }
+        }   )
+    
+    }
+    updateTime()
+    
+    
+    
+    })
 
 
 
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Work-Day-Scheduler-Project
-A simple calendar scheduler for tracking work tasks
+# Work Day Scheduler Project
+
+A simple calendar scheduler for tracking work tasks.
+
+Events in the current hour will display in red. Future hours are displayed in green, and past in grey.
+
+User can type an event name into a green field, then click the adjacent button to save that event.
+
+## -- Example appearance --
+
+![WS Example](assets/Work-Schedule-Example.PNG "Work Schedule Example")
+
+### -- *Functionality to be added* --
+
+* User will receive an error message when attempting to enter an event into a current or elapsed hour.
+
+* Data will be saved in local storage and retained upon page reload.
